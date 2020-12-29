d424bb9aa4d934510e0802857ad6cd491172ac77
Chen-Yu Pao <windperson@gmail.com>
2020-12-29 19:20:30 +0800

    add component library

---
+++ CounterComponent/Component1.razor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CounterComponent
+{
+    public partial class Component1
+    {
+        [Parameter] public string ParentName { get; set; }
+
+        [Inject] private IJSRuntime JsRuntime { get; set; }
+
+        private async Task GetTextAsync()
+        {
+            await using var js = new ExampleJsInterop(JsRuntime);
+            var text = await js.Prompt("Enter some text:");
+            
+            await TextChanged.InvokeAsync(text);
+            Text = text;
+        }
+    }
+}
